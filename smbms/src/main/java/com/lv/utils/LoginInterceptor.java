14631db8310817af8afbfe9abf43fbcdc437f6a0
superdoog <lvsihao@126.com>
2020-02-24 15:55:48 +0800

    拦截、事务

---
+++ smbms/src/main/java/com/lv/utils/LoginInterceptor.java
@@ -0,0 +1,32 @@
+package com.lv.utils;
+
+import com.lv.pojo.User;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        User user = (User) request.getSession().getAttribute(Constants.USER_SESSION);
+        if (user == null){ //session过期
+            //跳转到登录页面
+            request.getRequestDispatcher("/login.jsp").forward(request,response);
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
